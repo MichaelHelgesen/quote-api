9b04a7b1be8b1e10c913e3f87bf51db563d5bcf5
Michael Helgesen <michaelhelgesen02@gmail.com>
2024-09-24 10:03:28 +0200

    Add endpoints and forms for del and update

--- app.py
+++ app.py
@@ -27,6 +27,15 @@ class QuoteForm(FlaskForm):
     source = StringField("Source")
     submit = SubmitField("Submit")
 
+class UpdateQuoteForm(FlaskForm):
+    quote_id = StringField("Quote ID", validators=[DataRequired()])
+    quote = StringField("Quote", validators=[DataRequired()])
+    source = StringField("Source")
+    submit = SubmitField("Submit")
+
+class DeleteForm(FlaskForm):
+    person_id = StringField("Person ID", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 @app.route("/add_person", methods=["GET", "POST"])
 def home():
@@ -134,3 +143,98 @@ def get_quote(quote_id):
         return quotes[quote_id]
     except KeyError:
         abort(404, message = "Quote not found")
+
+@app.delete("/quote/<string:quote_id>")
+def delete_quote(quote_id):
+    try:
+        del quotes[quote_id]
+        return {"message": "Item deleted"}
+    except KeyError:
+        abort(404, message="Item not found")
+
+@app.route("/update_quote", methods=["GET", "POST"])
+def upd_quote():
+    quote_id = None
+    quote = None
+    source = None
+    form = UpdateQuoteForm()
+    if form.validate_on_submit():
+        quote_id = form.quote_id.data
+        source = form.source.data
+        quote = form.quote.data
+        if quote_id not in quotes:
+            flash("Quote not found")
+            return render_template("update_quote.html",
+                quote_id = quote_id,
+                quote = quote,
+                source = source,
+                form = form
+            )
+
+        new_quote = {"quote": quote, "source": source}
+        original_quote = quotes[quote_id]
+        original_quote |= new_quote
+        form.quote_id.data = ""
+        form.quote.data = ""
+        form.source.data = ""
+        flash("Quote updated")
+        return render_template("update_quote.html",
+            quote_id = quote_id,
+            quote = quote,
+            source = source,
+            form = form
+        )
+    return render_template("update_quote.html",
+        quote_id = quote_id,
+        quote = quote,
+        source = source,
+        form = form
+    )
+
+@app.put("/quote/<string:quote_id>")
+def update_quote(quote_id):
+    quote_data = request.get_json()
+    if "quote" not in quote_data or quote_data["quote"] == "":
+        abort(400, message="Bad request. Ensure quote is written")
+
+    try:
+        quote = quotes[quote_id]
+        quote |= quote_data
+
+        return quote
+    except KeyError:
+        abort(404, message="Quote not found")
+
+@app.route("/delete_person", methods=["GET", "POST"])
+def del_person():
+    person_id = None
+    form = DeleteForm()
+    if form.validate_on_submit():
+        person_id = form.person_id.data
+        try: 
+            del persons[person_id]
+            form.person_id.data = ""
+            flash("Person deleted")
+            return render_template("delete_person.html",
+                person_id = person_id,
+                form = form
+            )
+        except:
+            flash("Person not found")
+            return render_template("delete_person.html",
+                person_id = person_id,
+                form = form
+            )
+
+    return render_template("delete_person.html",
+        person_id = person_id,
+        form = form
+        )
+
+@app.delete("/person/<string:person_id>")
+def delete_person(person_id):
+    try:
+        del persons[person_id]
+        return {"message": "Person deleted"}
+    except KeyError:
+        abort(404, message="Person not found")
---
+++ del_person.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "ID: " ID 
+
+curl -X DELETE \
+    http://127.0.0.1:5000/person/$ID
+
+
