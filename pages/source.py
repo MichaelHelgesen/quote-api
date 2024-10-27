36bfe0f72c6cbfecb0a2cbf738bc83da60847748
MichaelHelgesen <michaelhelgesen02@gmail.com>
2024-10-27 18:12:16 +0100

    Make path for tags and sources

--- pages/source.py
+++ pages/source.py
@@ -3,6 +3,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 from models import QuoteModel, TagModel, PersonModel 
 from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy.sql import func
 from db import db
 from schemas import QuoteSchema, QuoteUpdateSchema
 from forms import QuoteForm
@@ -10,10 +11,11 @@ from forms import QuoteForm
 
 blp = Blueprint("Source", __name__, description="See quotes from source")
 
-@blp.route("/source/<string:id>")
+@blp.route("/source/<string:id>".replace("_", ""))
 class QuoteBySource(MethodView):
     def get(self, id):
         # tag = TagModel.query.get_or_404(id)
-        quotes = QuoteModel.query.filter(QuoteModel.source == id)
+        id_new = id.replace("_", " ")
+        quotes = QuoteModel.query.filter(func.lower(QuoteModel.source) == id_new)
         return render_template("quote_source.html", quotes = quotes)
 
